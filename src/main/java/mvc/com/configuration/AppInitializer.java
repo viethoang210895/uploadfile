dd2356cd3e95f6efa48b15671303ed1311825d02
viethoang9999 <viethoang9999>
2022-12-15 14:47:17 +0700

    uploadfile

---
+++ src/main/java/mvc/com/configuration/AppInitializer.java
@@ -0,0 +1,22 @@
+package mvc.com.configuration;
+
+import mvc.com.configuration.AppConfiguration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
